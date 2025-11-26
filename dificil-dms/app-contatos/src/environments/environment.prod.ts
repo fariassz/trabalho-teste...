7d16b327b186b5182a87cc076192bdcbaec8269f
fariassz <zigueira3108@gmail.com>
2025-11-25 21:40:42 -0300

    Add files via upload

---
+++ dificil-dms/app-contatos/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC5M-AEMv0NYfPrklE75j6CRP3JiFCqMkA",
+    authDomain: "projeto-contatoss.firebaseapp.com",
+    databaseURL: "https://projeto-contatoss-default-rtdb.firebaseio.com",
+    projectId: "projeto-contatoss",
+    storageBucket: "projeto-contatoss.firebasestorage.app",
+    messagingSenderId: "1006574636644",
+    appId: "1:1006574636644:web:b8ec01b16fb852e39a1971"
+  }
+};
---
+++ dificil-dms/app-contatos/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC5M-AEMv0NYfPrklE75j6CRP3JiFCqMkA",
+    authDomain: "projeto-contatoss.firebaseapp.com",
+    databaseURL: "https://projeto-contatoss-default-rtdb.firebaseio.com",
+    projectId: "projeto-contatoss",
+    storageBucket: "projeto-contatoss.firebasestorage.app",
+    messagingSenderId: "1006574636644",
+    appId: "1:1006574636644:web:b8ec01b16fb852e39a1971"
+  }
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
